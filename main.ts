8ed2cd882171c8e6d1d98a7406107c87e9225a43
COWZinnSPACE <92816614+COWZinnSPACE@users.noreply.github.com>
2021-12-02 13:47:00 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,8 +3,10 @@ radio.onReceivedString(function (receivedString) {
     basic.showString(receivedString)
 })
 input.onButtonPressed(Button.B, function () {
-    x = 34 + 35
-    radio.sendString("HELP, I've FALLEN and I CAN'T GET UP UwU")
+    x = 2 + 3
+    if (x == 5) {
+        radio.sendString("HELP, I've FALLEN and I CAN'T GET UP UwU")
+    }
 })
 basic.forever(function () {
     radio.setGroup(500)
