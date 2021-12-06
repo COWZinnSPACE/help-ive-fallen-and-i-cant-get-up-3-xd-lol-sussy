5b4950dc11392d7cb9a5044a93d1ebca4aed5a61
COWZinnSPACE <92816614+COWZinnSPACE@users.noreply.github.com>
2021-12-06 14:07:10 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,11 @@
 let x = 0
-radio.onReceivedString(function (receivedString) {
-    basic.showString(receivedString)
-})
 input.onButtonPressed(Button.B, function () {
     x = 2 + 3
     if (x == 5) {
-        radio.sendString("HELP, I've FALLEN and I CAN'T GET UP UwU")
+        radio.sendString("HELP >:(")
     }
+    music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
 })
 basic.forever(function () {
-    radio.setGroup(500)
+    radio.setGroup(50)
 })
